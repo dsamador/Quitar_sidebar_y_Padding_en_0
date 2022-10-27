120494c6040897f27ecf79e829bd570643ef30f3
David Amador <54488581+dsamador@users.noreply.github.com>
2022-10-27 11:40:35 -0500

    punto y coma

--- quitaSidebar.js
+++ quitaSidebar.js
@@ -1,8 +1,8 @@
-miBtn = document.querySelector('header').appendChild(document.createElement('button'))
-miBtn.textContent = 'Corregir'
-sideBar = document.getElementById("courseSidebar")
+miBtn = document.querySelector('header').appendChild(document.createElement('button'));
+miBtn.textContent = 'Corregir';
+sideBar = document.getElementById("courseSidebar");
 
 miBtn.addEventListener('click', ()=>{
-    sideBar.remove()
+    sideBar.remove();
     document.querySelector('.lecture-page-layout').children[0].style.padding = '16px 0px';
 });
