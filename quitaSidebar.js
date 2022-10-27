3f8595d82532bbc01f813d4a5bcbf4e172b382f0
David Amador <54488581+dsamador@users.noreply.github.com>
2022-10-27 11:39:41 -0500

    update sidebar

--- quitaSidebar.js
+++ quitaSidebar.js
@@ -1,8 +1,8 @@
 miBtn = document.querySelector('header').appendChild(document.createElement('button'))
 miBtn.textContent = 'Corregir'
-coso = document.getElementById("courseSidebar")
+sideBar = document.getElementById("courseSidebar")
 
 miBtn.addEventListener('click', ()=>{
-    coso.remove()
+    sideBar.remove()
     document.querySelector('.lecture-page-layout').children[0].style.padding = '16px 0px';
 });
