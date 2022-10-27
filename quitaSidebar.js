c581092c286ca57cf1919c189a900980b9d60689
David Amador <54488581+dsamador@users.noreply.github.com>
2022-10-27 11:39:10 -0500

    Ahora funciona con un botón

--- quitaSidebar.js
+++ quitaSidebar.js
@@ -1,7 +1,8 @@
+miBtn = document.querySelector('header').appendChild(document.createElement('button'))
+miBtn.textContent = 'Corregir'
 coso = document.getElementById("courseSidebar")
 
-setTimeout(()=>{
+miBtn.addEventListener('click', ()=>{
     coso.remove()
-    /* Version 1.1 */
     document.querySelector('.lecture-page-layout').children[0].style.padding = '16px 0px';
-}, 2000)
+});
